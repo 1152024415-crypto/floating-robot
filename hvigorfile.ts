72c4111b7f610a98c8b1832f8d9e4a49ecf0ed7a
xiaxingyu <xiaxingyu2@huawei.com>
2026-03-17 20:34:05 +0800

    sign config

--- hvigorfile.ts
+++ hvigorfile.ts
@@ -1,6 +1,21 @@
 import { appTasks } from '@ohos/hvigor-ohos-plugin';
+import { onlineSignPlugin } from '@ohos/hvigor-ohos-online-sign-plugin';
+import type { OnlineSignOptions } from '@ohos/hvigor-ohos-online-sign-plugin';
+
+const signOptions: OnlineSignOptions = {
+  profile: 'hwsign_system/1773386843532debug.p7b',
+  keyAlias: 'HOS Application Provision Debug V2',
+  hapSignToolFile: `${process.env.HAP_SIGN_TOOL ??
+    'hwsign_system/hap-sign-tool.jar'}`,
+  username: ` `,
+  password: ` `,
+  enableOnlineSign: true
+}
+
 
 export default {
-  system: appTasks, /* Built-in plugin of Hvigor. It cannot be modified. */
-  plugins: []       /* Custom plugin to extend the functionality of Hvigor. */
+  system: appTasks,
+  plugins:[
+    onlineSignPlugin(signOptions)
+  ]
 }
